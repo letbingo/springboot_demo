ce39d3e1949578eba45e6ffc840003a0d0e96b05
letbingo <584460397@qq.com>
2020-05-02 19:38:55 +0800

    无

--- spring boot-elasticsearch/src/main/java/com/lego/dao/PersonDAO.java
+++ spring boot-elasticsearch/src/main/java/com/lego/dao/PersonDAO.java
@@ -1,9 +1,8 @@
 package com.lego.dao;
- 
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
- 
+
 import com.lego.pojo.Person;
- 
-public interface PersonDAO extends ElasticsearchRepository<Person,Integer>{
- 
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface PersonDAO extends ElasticsearchRepository<Person, Integer> {
+
 }
